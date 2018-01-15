df1052039ae3ae6dd73e4c8a5d6e2ccf77d86b57
Seiya Nakamura <e175711@ie.u-ryukyu.ac.jp>
2018-01-15 17:20:53 +0900

    anytime commit

---
+++ src/test/java/mazeParts/mazeCreateTest.java
@@ -0,0 +1,16 @@
+package mazeParts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class mazeCreateTest {
+    @Test
+    public void createRoute() throws Exception {
+    }
+
+    @Test
+    public void canCreate() throws Exception {
+    }
+
+}
